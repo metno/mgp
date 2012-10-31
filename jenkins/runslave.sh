2faf4820fea9a3a7e088b04976f2e627e9f94611
Jo Asplin <jo.asplin@met.no>
2012-10-31 15:58:08 +0100

    Fixed bug

--- jenkins/runslave.sh
+++ jenkins/runslave.sh
@@ -8,6 +8,6 @@ jardir=$2
 ssh_options='-i /tmp/jenkins/.ssh/id_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
 
 source=$jardir/slave.jar
-target=$JENKINS_HOME/slave.jar
+target=$jardir/slave.jar
 scp $ssh_options $source root@$host:$target
 ssh $ssh_options root@$host java -jar $target
