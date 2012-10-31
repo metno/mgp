e43193789fe47ce6b0a3ae016936943e4d0a8121
Jo Asplin <jo.asplin@met.no>
2012-10-31 16:11:50 +0100

    Fixed bug

--- jenkins/runslave.sh
+++ jenkins/runslave.sh
@@ -4,10 +4,11 @@ set -e
 set -x
 
 host=$1
-jardir=$2
+jenkins_home=$2
+jenkins_scripts_path=$3
 ssh_options='-i /tmp/jenkins/.ssh/id_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
 
-source=$jardir/slave.jar
-target=$jardir/slave.jar
+source=$jenkins_scripts_path/slave.jar
+target=$jenkins_home/slave.jar
 scp $ssh_options $source root@$host:$target
 ssh $ssh_options root@$host java -jar $target
