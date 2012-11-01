96a9cb08bcc3105f8c8bdefe67acb2ad3a13605d
Jo Asplin <jo.asplin@met.no>
2012-11-01 15:34:41 +0100

    Dropped call to notifybuildfailure.py
    We will rely on the Blame Upstream Committers plugin in Jenkins
    for email notification to candidate committers.

--- jenkins/runjob.py
+++ jenkins/runjob.py
@@ -53,58 +53,28 @@ except:
     sys.stderr.write('failed to run core build script {}:\n{}\n'.format(script, format_exc()))
     sys.exit(1)
 
-xpass = False
+# Respond to result
 if p.returncode == 0:
-    # the core build script terminated successfully
     if 'expectfail' in options:
-        # the core build script was expected to fail, so mark incident to be reported
-        xpass = True
+        # unexpected pass
+        comment = finalExpectFailComment(options['expectfail'])
+        sys.stderr.write('error: the core build script ({}) passed unexpectedly: {}\n'.format(script, comment))
+        exit_code = 1
     else:
-        # the pass was expected, so we're done
-        printOutput(bs_stdout, bs_stderr)
-        sys.exit(0)
-
-# At this point the core build script either 1) failed or 2) passed unexpectedly
-
-cmd = [
-    os.environ['JENKINS_SCRIPTS_PATH'] + '/notifybuildfailure.py',
-    '--jenkinshome', os.environ['JENKINS_HOME'],
-    '--jenkinsurl', os.environ['JENKINS_URL'],
-    '--job', os.environ['JOB_NAME'],
-    '--build', os.environ['BUILD_NUMBER']
-    ]
-if xpass:
-    # unexpected pass
-    comment = finalExpectFailComment(options['expectfail'])
-    sys.stderr.write('error: the core build script ({}) passed unexpectedly: {}\n'.format(script, comment))
-    cmd += ['--xpass', comment]
+        # expected pass
+        exit_code = 0
 else:
     if 'expectfail' in options:
         # expected failure
         sys.stderr.write('the core build script ({}) failed (with exit code {}), but this was expected: {}\n'.format(
                 script, p.returncode, finalExpectFailComment(options['expectfail'])))
-        printOutput(bs_stdout, bs_stderr)
-        sys.exit(0) # don't flag this as an error
+        exit_code = 0
     else:
         # unexpected (i.e. "normal") failure
         sys.stderr.write('error: the core build script ({}) failed with exit code {}\n'.format(script, p.returncode))
-
-# At this point the pass or failure was unexpected
+        exit_code = 1
 
 printOutput(bs_stdout, bs_stderr)
-sys.stderr.write('notifying relevant people ...\n')
-
-try:
-    p = Popen(cmd, stdout = PIPE, stderr = PIPE)
-    nf_stdout, nf_stderr = p.communicate()
-    sys.stderr.write('notification succeeded\n')
-except:
-    sys.stderr.write('notification failed: {}\n'.format(format_exc()))
-    sys.exit(1)
-
-printOutput(nf_stdout, nf_stderr)
-
-# Indicate that this script as such failed (since the core build script result was unexpected)
-sys.exit(1)
+sys.exit(exit_code)
 
 # --- END main program ---------------------------------------
