1da8f9f8d15ed7c2e20271ed5349bd86aa147c7d
Jo Asplin <jo.asplin@met.no>
2012-11-07 21:45:34 +0100

    Added 'user' option

--- jenkins/runslave.sh
+++ jenkins/runslave.sh
@@ -3,12 +3,13 @@
 set -e
 set -x
 
-host=$1
-jenkins_home=$2
-jenkins_scripts_path=$3
+user=$1
+host=$2
+jenkins_home=$3
+jenkins_scripts_path=$4
 ssh_options='-i /tmp/jenkins/.ssh/id_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
 
 source=$jenkins_scripts_path/slave.jar
 target=$jenkins_home/slave.jar
 scp $ssh_options $source root@$host:$target
-ssh $ssh_options root@$host java -jar $target
+ssh $ssh_options $user@$host java -jar $target
