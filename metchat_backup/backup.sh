1ab2eb7b17c75c14e6e5c8c8450ce3829133b2da
Jo Asplin <jo.asplin@met.no>
2013-12-11 13:07:46 +0100

    Added metchat backup script

---
+++ metchat_backup/backup.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+dstdir=/home/joa/metchat_backup
+dbfile=qc.db
+
+# copy current DB file from server
+rsync -a root@metchat:/var/lib/qc/$dbfile $dstdir
+
+# if day of month is 1 or 15, copy current file to archived file
+# (note: we assume that the DB file contains messages 30 days back, so archiving twice
+# a month should be safe (i.e. provide plenty of overlap!))
+day=`date "+%d"`
+if [ $day -eq 1 ] || [ $day -eq 15 ]; then
+  cp $dstdir/$dbfile $dstdir/`date "+%Y-%m-%d"`-$dbfile
+fi
