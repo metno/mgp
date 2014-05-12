8f479460a2d437d99bb7342968ae696d39b94a0a
Jo Asplin <jo.asplin@met.no>
2014-05-12 14:19:52 +0200

    Return non-zero upon error

--- kml/main.cc
+++ kml/main.cc
@@ -18,7 +18,7 @@ int main(int argc, char *argv[])
 
   if ((argc < 2) || (argc > 3)) {
     printError(QString("usage: %1 <KML input file> [<KML output file>]").arg(argv[0]));
-    return 0;
+    return 1;
   }
 
   QString error;
