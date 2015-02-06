6d154eb845ea3aa2acd7877fc73ff2ffe45aec79
Jo Asplin <jo.asplin@met.no>
2015-02-06 15:07:50 +0100

    Highlight channels with id other than 1

--- metchat_extractor/metchat_extractor.py
+++ metchat_extractor/metchat_extractor.py
@@ -23,7 +23,7 @@ if len(sys.argv) < 4:
 timestamp_from = sys.argv[1]
 timestamp_to = sys.argv[2]
 
-channel_id = 1 # for now
+normal_channel_id = 1 # for now
 
 msg = {}
 
@@ -31,32 +31,30 @@ msg = {}
 for fname in sys.argv[3:]:
     con = sqlite3.connect(fname)
     rows = con.execute(
-        'SELECT id,timestamp,user,text FROM log WHERE timestamp >= ? AND timestamp <= ? ORDER BY timestamp;',
+        'SELECT id,timestamp,user,text,channelId FROM log WHERE timestamp >= ? AND timestamp <= ? ORDER BY timestamp;',
         (timestamp_from, timestamp_to))
 
-    msg_count = dup_count = 0
     for row in rows:
-        msg_count =msg_count + 1
-        id, timestamp, user, text = row[:]
-        m = [timestamp, user, text]
+        id, timestamp, user, text, channel_id = row[:]
+        m = [timestamp, user, text, channel_id]
         if id in msg:
-            dup_count = dup_count + 1
             if m != msg[id]:
                 sys.stderr.write('error: different contents found for same id\n')
                 sys.exit(1)
         else:
             msg[id] = m
 
-    #print 'file:', fname, ', messages:', msg_count, ', duplicates:', dup_count
-
-
 # write output in HTML format
 sys.stdout.write('<html><head><meta http-equiv="content-type" content="text/html;charset=utf-8" /></head><body><pre>\n')
 sorted_msg = sorted(msg.items(), key=lambda e: e[1][0]) # sort on timestamp
 for m in sorted_msg:
     m1 = m[1]
-    sys.stdout.write('{} {:10} {}\n'.format(
-            datetime.fromtimestamp(m1[0]).strftime('%Y-%m-%d %H:%M:%S'), m1[1], m1[2].encode('utf-8')))
+    prefix = suffix = ''
+    if m1[3] != normal_channel_id:
+        prefix = '<span style="background:#aff">'
+        suffix = '</span>'
+    sys.stdout.write('{}{} {:10} {}{}\n'.format(
+            prefix, datetime.fromtimestamp(m1[0]).strftime('%Y-%m-%d %H:%M:%S'), m1[1], m1[2].encode('utf-8'), suffix))
 sys.stdout.write('</pre></body></html>\n')
 
 sys.exit(0)
