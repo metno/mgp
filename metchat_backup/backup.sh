3ee3b57471fd723b4b905d5a49a999f924ae45df
Jo Asplin <jo.asplin@met.no>
2015-02-09 09:18:59 +0100

    Back up metchat images too

--- metchat_backup/backup.sh
+++ metchat_backup/backup.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
+srcdir=/var/lib/qc
 dstdir=/home/joa/metchat_backup
 dbfile=qc.db
 
 # copy current DB file from server
-rsync -a root@metchat:/var/lib/qc/$dbfile $dstdir
+rsync -a root@metchat:$srcdir/$dbfile $dstdir
 
 # if day of month is 1 or 15, copy current file to archived file
 # (note: we assume that the DB file contains messages 30 days back, so archiving twice
@@ -13,3 +14,6 @@ day=`date "+%d"`
 if [ $day -eq 1 ] || [ $day -eq 15 ]; then
   cp $dstdir/$dbfile $dstdir/`date "+%Y-%m-%d"`-$dbfile
 fi
+
+# copy images from server
+rsync -a root@metchat:$srcdir/images/ $dstdir/images
