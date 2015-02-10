2d6093518958a2021c6b020ceb20bd43f6d84f68
Jo Asplin <jo.asplin@met.no>
2015-02-10 11:00:27 +0100

    Made backup script more robust
    We no longer require the backup script to run on the 1st or 15th
    of a month in order to archive the current DB file. Instead, the
    script will archive if the latest archiving was made more than
    15 days ago, regardless of the current date.

--- metchat_backup/backup.sh
+++ metchat_backup/backup.sh
@@ -7,11 +7,12 @@ dbfile=qc.db
 # copy current DB file from server
 rsync -a root@metchat:$srcdir/$dbfile $dstdir
 
-# if day of month is 1 or 15, copy current file to archived file
-# (note: we assume that the DB file contains messages 30 days back, so archiving twice
-# a month should be safe (i.e. provide plenty of overlap!))
-day=`date "+%d"`
-if [ $day -eq 1 ] || [ $day -eq 15 ]; then
+# archive current DB file if more than 15 days have passed since the last archived file
+# (note: we assume that the DB file contains messages 30 days back, so archiving around
+# twice a month should be safe, i.e. provide plenty of overlap!)
+scriptdir=`dirname "$0"`
+echo $scriptdir
+if $scriptdir/newest_file_too_old.py 15 $dstdir/*-qc.db; then
   cp $dstdir/$dbfile $dstdir/`date "+%Y-%m-%d"`-$dbfile
 fi
 
---
+++ metchat_backup/newest_file_too_old.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+
+"""
+This program exits with zero (i.e. success) iff the newest file among the given input files is older
+than the specified number of days.
+
+Notes:
+
+  - The age of the file is determined from the name, not the actual modification time (which may
+    have changed since the file was created).
+
+  - The input files are assumed to be of the form <prefix>yyyy-mm-dd-qc.db
+    For example: /home/joa/metchat_backup/2015-01-24-qc.db
+"""
+
+import sys, os, re, time, datetime
+
+if len(sys.argv) < 2:
+    sys.stderr.write('usage: ' + sys.argv[0] + ' <max days tolerance> [file1 file2 ...]\n')
+    sys.exit(1)
+
+try:
+    max_days = int(sys.argv[1])
+except:
+    sys.stderr.write('failed to parse max days tolerance as integer\n')
+    sys.exit(1)
+
+# process input files and find newest timestamp
+newest_ts = -1
+p = re.compile('.*(\d\d\d\d)-(\d\d)-(\d\d)-qc.db$')
+for fname in sys.argv[2:]:
+    m = p.match(fname)
+    if m:
+        dt = datetime.datetime(year = int(m.group(1)), month = int(m.group(2)), day = int(m.group(3)))
+        newest_ts = max(newest_ts, time.mktime(dt.timetuple()))
+
+curr_ts = time.time()
+max_secs = 24 * 3600 * max_days
+
+sys.stderr.write('curr_ts: {}, newest_ts: {}, diff: {}, max_secs: {}\n'.format(
+        curr_ts, newest_ts, curr_ts - newest_ts, max_secs))
+
+if curr_ts - newest_ts > max_secs:
+    sys.exit(0) # newest file is too old
+
+sys.exit(1)
