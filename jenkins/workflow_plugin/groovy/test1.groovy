80167eaaff197ce6a72a40641bee0eb6202535b5
Jo Asplin <jo.asplin@met.no>
2015-02-12 14:50:23 +0100

    Updated script

--- jenkins/workflow_plugin/groovy/test1.groovy
+++ jenkins/workflow_plugin/groovy/test1.groovy
@@ -2,4 +2,8 @@ node('master') {
     echo 'checking out met-api.git in workspace on master ...';
     git url: 'https://github.com/metno/met-api.git';
     echo 'done';
+
+    def sleepHome = tool 'sleep';
+    echo 'sleepHome:' $sleepHome;
+
 }
