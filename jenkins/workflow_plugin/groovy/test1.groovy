3d9669ccaaf133973963fe182540e0ceccf00708
Jo Asplin <jo.asplin@met.no>
2015-02-13 12:59:57 +0100

    Updated script

--- jenkins/workflow_plugin/groovy/test1.groovy
+++ jenkins/workflow_plugin/groovy/test1.groovy
@@ -1,12 +1,8 @@
 // Execute test_a.py twice sequentially.
 node('metapps-cpp-slave1') {
 
-    //echo 'checking out met-api.git in workspace on metapps-cpp-slave1 ...';
     //git url: 'https://github.com/metno/met-api.git';
-    echo 'checking out joa.git in workspace on metapps-cpp-slave1 ...';
     git url: 'git://git.met.no/joa.git';
-
-    echo 'done';
 
     //input 'ready to continue?'
 
@@ -16,21 +12,18 @@ node('metapps-cpp-slave1') {
     sh 'jenkins/workflow_plugin/python/test_a.py 6 0.5'
 }
 
-node('ted') {
-    echo 'checking out joa.git in workspace on ted ...';
-    git url: 'git://git.met.no/joa.git';
-}
-
 // Execute test_a.py twice in parallel.
 def branches = [:]
 branches["branch >>> 0 <<<"] = {
     node('metapps-cpp-slave1') {
+        git url: 'git://git.met.no/joa.git';
         echo 'running test_a.py in parallel for 50 secs ...';
         sh 'jenkins/workflow_plugin/python/test_a.py 50 1.0'
     }
 }
 branches["branch >>> 1 <<<"] = {
     node('ted') {
+        git url: 'git://git.met.no/joa.git';
         echo 'running test_a.py in parallel for 30 secs ...';
         sh 'jenkins/workflow_plugin/python/test_a.py 60 0.5'
     }
@@ -38,3 +31,5 @@ branches["branch >>> 1 <<<"] = {
 
 echo 'starting parallel branches ...';
 parallel branches
+
+echo 'after parallel execution';
