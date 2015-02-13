e367392820cb681a52076bd5c7bb09a0138de3cb
Jo Asplin <jo.asplin@met.no>
2015-02-13 10:53:33 +0100

    Updated script

--- jenkins/workflow_plugin/groovy/test1.groovy
+++ jenkins/workflow_plugin/groovy/test1.groovy
@@ -1,9 +1,14 @@
 node('metapps-cpp-slave1') {
-    echo 'checking out met-api.git in workspace on metapps-cpp-slave1 ...';
-    git url: 'https://github.com/metno/met-api.git';
+
+    //echo 'checking out met-api.git in workspace on metapps-cpp-slave1 ...';
+    //git url: 'https://github.com/metno/met-api.git';
+    echo 'checking out joa.git in workspace on metapps-cpp-slave1 ...';
+    git url: 'git://git.met.no/joa.git';
+
     echo 'done';
 
     //input 'ready to continue?'
+
     echo 'continuing to run test_a.py ...';
     echo 'PWD: $PWD'
     sh 'echo $PWD'
