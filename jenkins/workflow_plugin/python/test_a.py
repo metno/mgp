22e3310f0b0d1f9e1af514c0efa6c8210e3b5cbf
Jo Asplin <jo.asplin@met.no>
2015-02-13 14:32:26 +0100

    Updated script

--- jenkins/workflow_plugin/python/test_a.py
+++ jenkins/workflow_plugin/python/test_a.py
@@ -9,14 +9,14 @@ try:
 except:
     sys.stderr.write('\
 usage: {} <n iterations> <secs to sleep in each iteration> \
-[<iteration after which to fail with exit code 1 (0 = after first iteration etc.)>]\n'.format(
+[<iteration after which to fail with exit code 1 (1 = after first iteration etc.)>]\n'.format(
             sys.argv[0]))
     sys.exit(1)
 
 sys.stderr.write('test_a.py running; pid:{} ...\n'.format(os.getpid()))
 
 try:
-    fail_iter = int(sys.argv[3])
+    fail_iter = int(sys.argv[3]) - 1
 except:
     fail_iter = -1 # default, i.e. never fail
 
