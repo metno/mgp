586b5c68635aa9aa51467f07b855e3f9e7ce4215
Jo Asplin <jo.asplin@met.no>
2015-02-13 10:20:35 +0100

    Updated script

--- jenkins/workflow_plugin/groovy/test1.groovy
+++ jenkins/workflow_plugin/groovy/test1.groovy
@@ -1,5 +1,5 @@
-node('master') {
-    echo 'checking out met-api.git in workspace on master ...';
+node('metapps-cpp-slave1') {
+    echo 'checking out met-api.git in workspace on metapps-cpp-slave1 ...';
     git url: 'https://github.com/metno/met-api.git';
     echo 'done';
 
