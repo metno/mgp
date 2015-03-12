27cc1f6188ef95dce79003f0d54b9f6130789085
Jo Asplin <jo.asplin@met.no>
2015-03-12 14:50:15 +0100

    Updated examples

---
+++ scala/test11.scala
@@ -0,0 +1,35 @@
+trait Ord {
+  def < (that: Any): Boolean
+  def <=(that: Any): Boolean =  (this < that) || (this == that)
+  def > (that: Any): Boolean = !(this <= that)
+  def >=(that: Any): Boolean = !(this < that)
+}
+
+class Date(y: Int, m: Int, d: Int) extends Ord {
+  def year = y
+  def month = m
+  def day = d
+  override def toString(): String = year + "-" + month + "-" + day
+
+  override def equals(that: Any): Boolean =
+    that.isInstanceOf[Date] && {
+      val o = that.asInstanceOf[Date]
+      o.day == day && o.month == month && o.year == year
+    }
+
+  def <(that: Any): Boolean = {
+    if (!that.isInstanceOf[Date])
+      sys.error("cannot compare " + that + " and a Date")
+    val o = that.asInstanceOf[Date]
+    (year < o.year) || (year == o.year && (month < o.month || (month == o.month && day < o.day)))
+  }
+}
+
+object Test11 {
+  def main(args: Array[String]) {
+    val d1 = new Date(2015, 04, 11)
+    val d2 = new Date(2015, 05, 11)
+    val b = d1 < d2
+    println(b)
+  }
+}
