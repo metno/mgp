c04be053b1b7ea3ba9f6d8ee6636b290747649cc
Jo Asplin <jo.asplin@met.no>
2015-04-15 09:38:05 +0200

    Scroll view after scaling

--- timeline/laneview.cpp
+++ timeline/laneview.cpp
@@ -5,6 +5,8 @@
 #include "wheelscaler.h"
 #include "common.h"
 #include <QResizeEvent>
+#include <QScrollBar>
+#include <QRectF>
 
 LaneView::LaneView(LaneScene *scene, QWidget *parent)
     : QGraphicsView(scene, parent)
@@ -30,10 +32,21 @@ void LaneView::wheelEvent(QWheelEvent *event)
 {
     const QPair<qreal, qreal> scaleFactors = WheelScaler::exec(this, event);
     if (scaleFactors.first > 0) {
+        const QPoint vpos = mapFromGlobal(QCursor::pos());
+        const QPointF spos = mapToScene(vpos);
+        const QRectF srect = scene()->sceneRect();
+        const qreal hsmfrac = (spos.x() - srect.x()) / srect.width(); // horizontal scene frac at mouse pos
+        const qreal vsmfrac = (spos.y() - srect.y()) / srect.height(); // vertical scene frac at mouse pos
+
         const qreal sx = scaleFactors.first;
         const qreal sy = scaleFactors.second;
-        setTransformationAnchor(QGraphicsView::AnchorUnderMouse);
         updateScale(sx, sy);
+
+        // update view to focus on the part of the scene that was under the mouse before scaling
+        // ### this feels only partially intuitive, but maybe it is good enough?
+        horizontalScrollBar()->setValue(horizontalScrollBar()->minimum() + hsmfrac * (horizontalScrollBar()->maximum() - horizontalScrollBar()->minimum()));
+        verticalScrollBar()->setValue(verticalScrollBar()->minimum() + vsmfrac * (verticalScrollBar()->maximum() - verticalScrollBar()->minimum()));
+
     } else {
         QGraphicsView::wheelEvent(event);
     }
