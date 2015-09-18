1d2ad42f592d4b4a87b1e358c795c8ea7ea93eb3
Jo Asplin <jo.asplin@met.no>
2015-09-18 13:54:32 +0200

    Modified example

--- misc/boost/format/main.cpp
+++ misc/boost/format/main.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    std::cout << boost::format("writing %1%,  x=%2% : %3%-th try\n") % "toto" % 40.23 % 50; 
-    // prints "writing toto,  x=40.230 : 50-th try"
+    std::cout << boost::format("writing %1%,  x=%2% : %1% %3%-th try\n") % "toto" % 40.23 % 50;
+    // prints "writing toto,  x=40.230 : toto 50-th try"
     return 0;
 }
