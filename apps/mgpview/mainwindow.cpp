c0adb0c83854adecb5ac31ef318bd0023933ea00
Jo Asplin <jo.asplin@met.no>
2016-03-17 14:00:58 +0100

    Updated version in main window title

--- apps/mgpview/mainwindow.cpp
+++ apps/mgpview/mainwindow.cpp
@@ -30,7 +30,7 @@ MainWindow &MainWindow::instance()
 
 MainWindow::MainWindow()
 {
-    setWindowTitle("MGPView 1.1.3");
+    setWindowTitle("MGPView 1.1.4");
 
     QGridLayout *mainLayout = new QGridLayout;
 
