185a43587a71601b98f656a3e715e05a54cd4d79
Jo Asplin <jo.asplin@met.no>
2016-05-24 13:14:19 +0200

    Bumped package version

--- apps/mgpview/mainwindow.cpp
+++ apps/mgpview/mainwindow.cpp
@@ -30,7 +30,7 @@ MainWindow &MainWindow::instance()
 
 MainWindow::MainWindow()
 {
-    setWindowTitle("MGPView 1.1.9");
+    setWindowTitle("MGPView 1.1.10");
 
     QGridLayout *mainLayout = new QGridLayout;
 
